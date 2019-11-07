8b9bff5c09b9515f5dd594daa90d344a1b572792
Roger Winright <rogerwinright@gmail.com>
2019-11-06 21:35:48 -0500

    started adding enemies

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,6 @@
-import { init, initPointer, initKeys, pointer, Sprite, GameLoop, keyPressed } from 'kontra';
+import { init, initPointer, initKeys, pointer, Sprite, GameLoop, keyPressed, setStoreItem, getStoreItem } from 'kontra';
 import pointerTools from './scripts/pointer-tools';
-import movement from './scripts/movement-controls';
+import movement from './scripts/player-movement-controls';
 
 let { canvas, context } = init();
 initPointer();
@@ -17,13 +17,28 @@ let sprite = Sprite({
   speed: 3
 });
 
+let blocks = Sprite({
+  x: 200,        // starting x,y position of the sprite
+  y: 0,
+  color: 'brown',
+  width: 40,
+  height: 20,
+  dy: 2
+})
+
 let loop = GameLoop({
   update: function() { 
     sprite.update();
+    blocks.update();
     movement(keyPressed, sprite);
+
+    if(blocks.y >= canvas.height){
+      blocks.y = 0;
+    }
   },
   render: function() {
     sprite.render();
+    blocks.render();
     //Pointer tools
    pointerTools(context, pointer, canvas); 
   }
