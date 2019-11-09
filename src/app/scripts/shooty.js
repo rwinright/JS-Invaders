4be5d22a16e0bc8ebb608f96c57985ab197a4826
Roger Winright <rogerwinright@gmail.com>
2019-11-09 02:17:50 -0500

    added spinny bullets

--- src/app/scripts/shooty.js
+++ src/app/scripts/shooty.js
@@ -4,7 +4,8 @@ const shooty = (player, bulletPool, keyPressed) => {
       x: player.x + 7,
       y: player.y,
       color: 'blue',
-      width: 5,
+      width: 10,
+      anchor: {x:0.5, y:0.5},
       height: 5,
       dy: -4,
       ttl: Infinity,
@@ -15,6 +16,8 @@ const shooty = (player, bulletPool, keyPressed) => {
 
   //Set the "Time To Live" to 0 to refresh the pool count.
   b.forEach((blt) => {
+    blt.rotation += 2
+    console.log(blt.rotation)
     if (blt.y <= 0 /*|| blt.collidesWith(player) Change to enemy block later */) {
       blt.ttl = 0;
     }
