3184fcad5f1d6afd1ebfe2f2b97b99a1c38a4d24
Roger Winright <rogerwinright@gmail.com>
2019-11-08 22:58:41 -0500

    Added pooling test code

--- src/app/main.js
+++ src/app/main.js
@@ -1,4 +1,4 @@
-import { init, initPointer, initKeys, pointer, Sprite, GameLoop, keyPressed, Vector } from 'kontra';
+import { init, initPointer, initKeys, pointer, Sprite, GameLoop, keyPressed, Vector, Pool } from 'kontra';
 import pointerTools from './scripts/pointer-tools';
 import movement from './scripts/player-movement-controls';
 
@@ -6,7 +6,7 @@ let { canvas, context } = init();
 initPointer();
 initKeys();
 
-const spriteVector = Vector(200, 400)
+const spriteVector = Vector(canvas.width/2 - 10, 400);
 let sprite = Sprite({
   x: spriteVector.x,        // starting x,y position of the sprite
   y: spriteVector.y,
@@ -15,31 +15,56 @@ let sprite = Sprite({
   height: 40,
   speed: 3
 });
+spriteVector.clamp(0, canvas.height/2 + sprite.height, canvas.width - sprite.width, canvas.height - sprite.height);
 
-let blocks = Sprite({
-  x: 200,        // starting x,y position of the sprite
-  y: 0,
-  color: 'brown',
-  width: 40,
-  height: 20,
-  dy: 2
-})
+// let blocks = Sprite({
+//   x: 200,        // starting x,y position of the sprite
+//   y: 0,
+//   color: 'brown',
+//   width: 40,
+//   height: 20,
+//   dy: 2
+// })
+
+//Set the pool to create things.
+//Can be either an object or a sprite
+// let blockPool = Pool({
+//   create: Sprite,
+//   maxSize: 1 //Adjust this to set the number of pooled sprites on the screen at once. 
+// });
 
 let loop = GameLoop({
-  update: function() { 
+  update: function () {
     movement(keyPressed, sprite, spriteVector);
     sprite.update();
-    blocks.update();
+    // blocks.update();
 
-    if(blocks.y >= canvas.height){
-      blocks.y = 0;
-    }
+    //Add blocks to the pool with the speficied params.
+    // blockPool.get({
+    //   x: Math.floor(Math.random() * canvas.width) + 1,        // starting x,y position of the sprite
+    //   y: 0,
+    //   color: 'brown',
+    //   width: 40,
+    //   height: 20,
+    //   dy: 2,
+    //   ttl: 280
+    // })
+
+    // blockPool.update();
+
+    // blocks.collidesWith(sprite) ? (console.log("Collided with player"), blocks.dy = 0) : blocks.dy = 2;
+
+    // if(blocks.y >= canvas.height){
+    //   blocks.y = 0;
+    //   //Randomly spawn between random number and the width of the canvas
+    //   blocks.x = Math.floor(Math.random() * canvas.width) + 1
+    // }
   },
-  render: function() {
+  render: function () {
     sprite.render();
-    blocks.render();
+    // blockPool.render();
     //Pointer tools
-   pointerTools(context, pointer, canvas); 
+    pointerTools(context, pointer, canvas);
   }
 });
 
