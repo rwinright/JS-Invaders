888abb9fefc3d8f5c411b1740846be853e99f13a
Roger Winright <rogerwinright@gmail.com>
2019-11-19 19:37:04 -0500

    updated NPM, added sounds, modified dist

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
           }
         ]
       },{
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|ogg)$/i,
         use: [
           {
             loader: 'file-loader',
